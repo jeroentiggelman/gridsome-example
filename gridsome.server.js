58017318a4bf85551aa5cbf22b22ed583285ae13
jeroentiggelman <infos@photographicdesign.be>
2020-05-11 10:38:02 +0200

    cleaned up gridsome.server.js

--- gridsome.server.js
+++ gridsome.server.js
@@ -28,29 +28,3 @@ module.exports = function(api) {
     }
   });
 };
-// const axios = require("axios");
-
-// module.exports = function(api) {
-//   api.loadSource(async (store) => {
-//     const { data } = await axios.get(
-//       "https://www.reddit.com/r/aww.json?raw_json=1"
-//     );
-
-//     const contentType = store.addContentType({
-//       typename: "ReddidPost",
-//       route: "/reddit/:id",
-//     });
-
-//     for (const post of data.data.children) {
-//       contentType.addNode({
-//         id: post.data.id,
-//         title: post.data.title,
-//         path: "/reddit/" + post.data.id,
-//         fields: {
-//           thumbnail: post.data.thumbnail,
-//           img: post.data.preview.images[0].source.url,
-//         },
-//       });
-//     }
-//   });
-// };
